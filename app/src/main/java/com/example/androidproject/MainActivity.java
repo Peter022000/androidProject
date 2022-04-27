221acad1284fdb4ad9381ea9379c26900a66e6f1
Makolojka <101599455+Makolojka@users.noreply.github.com>
2022-04-27 16:54:15 +0200

    Revert "login system"

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,55 +2,13 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Toast;
-
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Disables action bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_main);
-
-        Button logIn = (Button) findViewById(R.id.logIn);
-        logIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNewActivity();
-            }
-        });
-    }
-
-    public void openNewActivity(){
-        Intent intent = new Intent(this, NavMenuActivity.class);
-        startActivity(intent);
-    }
-
-
-    //Go to SignUp acitivity
-    public void signUp(View view)
-    {
-        Intent intent = new Intent(this, SignUp.class);
-        startActivity(intent);
-    }
-
-    //Go to forgotPasswordAcitivity
-    public void forgotPassword(View view)
-    {
-        Intent intent = new Intent(this, ForgotPassword.class);
-        startActivity(intent);
     }
 }
