46c06559785640808657170995029a2e6865718a
Makolojka <marcinkrol1@onet.pl>
2022-04-30 00:16:55 +0200

    feature_profile
    Added:
    - profile cosmetics and new features
    - new database method

--- app/src/main/java/com/example/androidproject/ProfileActivity.java
+++ app/src/main/java/com/example/androidproject/ProfileActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,7 +12,13 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -20,6 +27,10 @@ public class ProfileActivity extends AppCompatActivity {
     Button button;
     Button loggedIn;
     Button loggedOut;
+    TextView loginField;
+    TextView emailField;
+    DatabaseHelper db;
+//    ArrayList<String> login, email, password, security_question, security_answer, profile_image_url, money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +45,30 @@ public class ProfileActivity extends AppCompatActivity {
         button = findViewById(R.id.logoutButton);
         loggedOut = findViewById(R.id.loggedOut);
         loggedIn = findViewById(R.id.loggedIn);
+        loginField = findViewById(R.id.loginField);
+        emailField = findViewById(R.id.emailField);
 
-        //Boolean ifChecked = checkBox.isChecked();
-        //Log.i("Checkbox checked: ", String.valueOf(ifChecked));
+        //Sets login field
+        loginField.setText(preferences.getString("LOGIN_KEY", ""));
 
+        db = new DatabaseHelper(ProfileActivity.this);
+
+//        login = new ArrayList<>();
+//        email = new ArrayList<>();
+//        password = new ArrayList<>();
+//        security_question = new ArrayList<>();
+//        security_answer = new ArrayList<>();
+//        profile_image_url = new ArrayList<>();
+//        money = new ArrayList<>();
+
+        //Returns and set user data
+        ArrayList<String> userData = new ArrayList<>();
+        userData = db.returnUserData(loginField.getText().toString());
+
+        emailField.setText(userData.get(1));
+
+
+        //Sets system to keep user logged
         loggedIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,6 +78,7 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        //Clears all preferences
         loggedOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,6 +90,7 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
 
+        //Logout method
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,6 +100,27 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
+
+//    void setUserData()
+//    {
+//        Cursor cursor = db.findRow(loginField.getText().toString());
+//        if(cursor.getCount() == 0)
+//        {
+//            Toast.makeText(getApplicationContext(), "No data.", Toast.LENGTH_SHORT).show();
+//        }
+//        else
+//        {
+//            while (cursor.moveToNext())
+//            {
+//                login.add(cursor.getString(0));
+//                email.add(cursor.getString(1));
+//                password.add(cursor.getString(2));
+//                security_question.add(cursor.getString(3));
+//                security_answer.add(cursor.getString(4));
+//                profile_image_url.add(cursor.getString(5));
+//                money.add(cursor.getString(6));
+//            }
+//        }
+//    }
 }
